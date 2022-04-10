4546d272e20276582c06038e68ba65a3f3a88f65
hyin8 <51980614+hyin8@users.noreply.github.com>
2022-04-10 14:04:38 -0700

    Server Skeleton
    source:https://www.geeksforgeeks.org/socket-programming-in-java/
    Using it as a starting point.

--- src/mapster/server/Server.java
+++ src/mapster/server/Server.java
@@ -1,7 +1,47 @@
 package mapster.server;
+import java.net.*;
+import java.io.*;
 
+//create a server that listens for connections
 public class Server {
-    public static void main(String[] args) {
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private int port = 0;
+    private DataInputStream in = null;
 
+    public Server(int port) {
+        try{
+            this.port = port;
+            server = new ServerSocket(port);
+            System.out.println("Server Started");
+
+            socket = server.accept();
+            System.out.println("Client Connected");
+
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+            while(!line.equals("exit")) {
+                try{
+                    line = in.readUTF();
+                    System.out.println(line);
+                }
+                catch(IOException i){
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            //close connection
+            socket.close();
+            in.close();
+        }
+        catch(IOException e){
+            System.out.println("Could not listen on port: " + port);
+            System.exit(1);
+        }
+    }
+    public static void main(String[] args) {
+        Server server = new Server(5000);
     }
 }
