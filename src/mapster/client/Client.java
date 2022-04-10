96f8216e808b231d22fed46f97ad44d2d8e9586d
voidstarr <voidstarr@users.noreply.github.com>
2022-04-10 13:46:32 -0700

    start project skeleton

---
+++ src/mapster/client/Client.java
@@ -0,0 +1,7 @@
+package mapster.client;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/mapster/messages/JoinMessage.java
@@ -0,0 +1,16 @@
+package mapster.messages;
+
+import java.io.Serializable;
+
+public class JoinMessage implements Serializable {
+    private final int port;
+
+    public JoinMessage(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
+
