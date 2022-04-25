a97b3d9889a60eea578938b9000369b8fdd8ac5f
hyin8 <51980614+hyin8@users.noreply.github.com>
2022-04-25 00:26:08 -0700

    update server.java to use methods

--- src/mapster/server/Server.java
+++ src/mapster/server/Server.java
@@ -9,88 +9,91 @@ public class Server {
     private Socket socket;
     private ServerSocket server;
     private int port;
-    private DataInputStream in;
+    //Streams for reading and writing
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
 
+    //Initialize streams that reads from the port
     private void initializeVariable(){
-        port = 0;
+        try{
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+            in = new ObjectInputStream(inputStream);
+            out = new ObjectOutputStream(outputStream);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     private String getCMDLineArguements(){
-        return "";
+        String cmdLine = "Connect to port: " + port;
+        return cmdLine;
     }
-    private void initializeSockets(){
-        try{
-            server = new ServerSocket(port);
-            socket = server.accept();
-            in = new DataInputStream(socket.getInputStream());
-        }catch(IOException e){
-            System.out.println("Error: " + e);
-        }
-    }
-    private void commandLoop(){
-        String cmd = "";
-        while(!cmd.equals("exit")){
-            try{
-                cmd = in.readUTF();
-                System.out.println(cmd);
-            }catch(IOException e){
-                System.out.println("Error: " + e);
-            }
-        }
-    }
-
-    public Server(int port) {
+    private void initializeSockets(int port){
         try{
             this.port = port;
             server = new ServerSocket(port);
             System.out.println("Server Started");
-
-            socket = server.accept();
-            System.out.println("Client Connected");
-
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-
-            String line = "";
-            while(!line.equals("exit")) {
-                try{
-                    line = in.readUTF();
-                    System.out.println(line);
-                }
-                catch(IOException i){
-                    System.out.println(i);
-                }
-            }
-            System.out.println("Closing connection");
-
-            //close connection
-            socket.close();
-            in.close();
         }
         catch(IOException e){
             System.out.println("Could not listen on port: " + port);
             System.exit(1);
         }
     }
+    private void commandLoop(){
+        try{
+            while(true){
+                //Accept a connection
+                socket = server.accept();
+                initializeVariable();
+                //Get the command line arguments
+                String cmdLine = getCMDLineArguements();
+                //Send the command line arguments to the client
+                out.writeObject(cmdLine);
+                out.flush();
+                //Get the response from the client
+                Object message = in.readObject();
+                System.out.println(message.toString());
+                if(message instanceof JoinMessage){
+                    JoinMessage command = (JoinMessage)message;
+                    System.out.printf("Received JoinMessage client port: %d.", ((JoinMessage)message).getPort());
+                }
+                else if(message instanceof LeaveMessage){
+                    LeaveMessage command = (LeaveMessage)message;
+                    System.out.printf("Received LeaveMessage");
+                    break;
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void closeSockets(){
+        try{
+            in.close();
+            out.close();
+            socket.close();
+            server.close();
+            inputStream.close();
+            outputStream.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //constructor
+    //input: port #
+    public Server(int port) {
+        initializeSockets(port);
+        commandLoop();
+        closeSockets();
+    }
+    public void startServer(){
+        commandLoop();
+    }
     public static void main(String[] args) throws IOException, ClassNotFoundException{
-        ServerSocket ss = new ServerSocket(5050);
-        System.out.println("ServerSocket awaiting connections...");
-        Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on this port.
-        System.out.println("Connection from " + socket + "!");
-
-        // get the input stream from the connected socket
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream = socket.getOutputStream();
-        // create a DataInputStream so we can read data from it.
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        // https://stackoverflow.com/questions/8088557/getinputstream-blocks
-        objectOutputStream.flush(); // THIS IS IMPORTANT AFTER EVERY OPERATION WITH A ObjectOutputStream
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-
-        // read the list of messages from the socket
-        Object message = objectInputStream.readObject();
-        System.out.println(message.toString());
-        if(message instanceof JoinMessage)
-        System.out.printf("Received JoinMessage client port: %d.", ((JoinMessage)message).getPort());
-        ss.close();
-        socket.close();
+        Server server = new Server(5050);
+        server.startServer();
     }
 }
